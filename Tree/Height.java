23263674f2e11011304755e4dddeb0cc43b32b00
sanskarfcb <sanskarshashwat.vns@gmail.com>
2025-05-21 20:28:01 +0530

    Solution of height of binary tree

---
+++ Tree/Height.java
@@ -0,0 +1,19 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class Height {
+
+        public int maxDepth(TreeNode root) {
+            if(root==null){
+                return 0;
+            }
+            if(root.left == null && root.right == null){
+                return 1;
+            }
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left,right)+1;
+        }
+    }
+
