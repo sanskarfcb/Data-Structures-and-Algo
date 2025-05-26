23eb7889ffcfdb0542aa363474ddf1d21143b770
sanskarfcb <sanskarshashwat.vns@gmail.com>
2025-05-26 13:14:47 +0530

    Solution for same Tree

---
+++ Tree/SameTree.java
@@ -0,0 +1,17 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class SameTree {
+
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if(p==null && q==null){
+                return true;
+            }
+            if(p != null && q !=null && p.val == q.val){
+                return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+            }
+            return false;
+        }
+    }
+
