be237bcb3cc00dba21ae4bb34bac69f6d0d9fb94
sanskarfcb <sanskarshashwat.vns@gmail.com>
2025-06-03 21:54:36 +0530

    Solution for LCA

---
+++ Tree/LowestCommonAncestor.java
@@ -0,0 +1,22 @@
+package Tree;
+
+import javax.swing.tree.TreeNode;
+
+public class LowestCommonAncestor {
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root == null ){
+                return null;
+            }
+            if(root == p || root == q){
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left , p , q);
+            TreeNode right = lowestCommonAncestor(root.right , p ,q);
+            if(left != null && right !=null){
+                return root;
+            }
+            return left!=null?left:right;
+        }
+    }
+}
