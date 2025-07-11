25afafd0549da03025b1b38222628108fe366860
Sanskar Mishra <sanskarshashwat.vns@gmail.com>
2025-07-11 12:24:04 +0530

    Solution for partition list

---
+++ LinkedList/partitionList.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+import java.lang.classfile.components.ClassPrinter;
+
+public class partionList {
+
+        public ClassPrinter.ListNode partition(ListNode head, int x) {
+            ListNode lessX = new ListNode(0);
+            ListNode moreX = new ListNode(0);
+
+            ListNode less = lessX;
+            ListNode more = moreX;
+
+            while(head!=null){
+                if(head.val <x){
+                    less.next =head;
+                    less = less.next;
+                }else{
+                    more.next = head;
+                    more = more.next;
+                }
+                head = head.next;
+            }
+            more.next = null;
+            less.next = moreX.next;
+
+            return lessX.next;
+        }
+    }
+
